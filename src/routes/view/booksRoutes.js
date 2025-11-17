e276d31c617e6641c4e49ba14cb8190bb7267cec
Julia Fokanova <oncologmma@mail.ru>
2025-11-17 15:02:12 +0300

    feat: change REST API, update packages

--- src/routes/view/booksRoutes.js
+++ src/routes/view/booksRoutes.js
@@ -7,18 +7,18 @@ const router = express.Router();
 router.get('/create', bookController.view.renderCreateBook);
 
 // GET -> форма редактирования книги:
-router.get('/update/:id', bookController.view.renderUpdateBook);
+router.get('/:id/update', bookController.view.renderUpdateBook);
 
 // GET -> скачивание книги (перед! динамическим id):
 router.get('/:id/download', bookController.actions.downloadBook);
+
+// POST (DELETE нельзя отправить через форму в HTML) -> удаление книги:
+router.post('/:id/delete', bookController.actions.deleteBook);
 
 // GET -> просмотр одной книги:
 router.get('/:id', bookController.view.renderBookById);
 
 // GET -> список всех книг:
 router.get('/', bookController.view.renderAllBooks);
-
-// POST (DELETE нельзя отправить через форму в HTML) -> удаление книги:
-router.post('/delete/:id', bookController.actions.deleteBook);
 
 export default router;
